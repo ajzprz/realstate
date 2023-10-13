ee73d25deb118bf2e474564c3cfb27afa064ab62
Ajaya Prajapati <ajz.prz@gmail.com>
2023-10-13 04:27:30 -0400

    mongoose added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
-import express, { response } from 'express';
+import express from 'express';
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+
+dotenv.config()
 
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Database connected successfully")
+}).catch((error)=>{
+    console.log(error)
+})
 
 app.get('/',(request, response)=>{
     return response.status(200).send("home page")

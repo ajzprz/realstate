5d53196e21757b7a4644b2841ce7df19ca58506b
Ajaya Prajapati <ajz.prz@gmail.com>
2023-10-30 01:48:41 -0400

    created api search route

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -3,13 +3,11 @@ import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
-import "swiper";
+import "swiper/css/bundle";
 import {
   FaBath,
   FaBed,
   FaChair,
-  FaMap,
-  FaMapMarker,
   FaMapMarkerAlt,
   FaParking,
 } from "react-icons/fa";
--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -4,11 +4,13 @@ import {
   deleteListing,
   getListing,
   updateListing,
+  getListings
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verify.js";
 
 const router = express.Router();
 
+router.get("/get", getListings);
 router.get("/get/:id", verifyToken, getListing);
 router.post("/create", verifyToken, createListing);
 router.post("/update/:id", verifyToken, updateListing);

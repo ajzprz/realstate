2d1ab0d32362d14fce129b4cd76337674fe3ded7
Ajaya Prajapati <ajz.prz@gmail.com>
2023-11-01 03:29:41 -0400

    home fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -56,7 +56,7 @@ const Home = () => {
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
           Find Your Next <span className="text-slate-500">Perfect</span> <br />
-          place wiht ease
+          place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
           E-state is the best place to find your next perfect place to live.{" "}
